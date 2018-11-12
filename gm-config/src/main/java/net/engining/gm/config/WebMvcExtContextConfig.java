71d5abd3bc25a353e40e54170a5c5c2d091e83c2
crazythinking <crazy_thinking@163.com>
2018-11-12 18:16:35 +0800

    add CommonsMultipartResolver replace Multipart auto config

--- gm-config/src/main/java/net/engining/gm/config/WebMvcExtContextConfig.java
+++ gm-config/src/main/java/net/engining/gm/config/WebMvcExtContextConfig.java
@@ -33,4 +33,5 @@ public class WebMvcExtContextConfig implements WebMvcConfigurer{
 			}
 		}
 	}
+
 }
--- gm-config/src/main/java/net/engining/gm/config/WebSecurityExtContextConfig.java
+++ gm-config/src/main/java/net/engining/gm/config/WebSecurityExtContextConfig.java
@@ -25,8 +25,14 @@ public class WebSecurityExtContextConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
-				.and().logout().permitAll();
+		http
+			.authorizeRequests()
+			.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/login").permitAll()
+			.and()
+			.logout().permitAll();
 	}
 
 	@Override
